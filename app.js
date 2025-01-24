468e2a60fdfa8c1430dfd483159a53b8409c5b19
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-01-24 14:31:47 +0200

    Sever

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-//Let's Create an express js server
-const express=require('express');
-const port=8080;
-const app=express();
-app.get('/',(req,res)=>{
-     res.send("Hello Express");
+// //Let's Create an express js server
+// const express=require('express');
+// const port=8081;
+// const app=express();
+// app.get('/',(req,res)=>{
+//      res.send("Hello Express");
+// })
+// app.listen(port,()=>{
+//      console.log("Server is running on http://localhost:8080")
+// })
+
+
+
+
+
+
+// Let's Create an Express.js server
+const express = require('express');
+const port = 1000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello Express");
+});
+app.get('/home',()=>{
+     res.send("Welcome to Home Page");
 })
-app.listen(port,()=>{
-     console.log("Server is running on http://localhost:8080")
+app.post('/login',()=>{
+     res.send("Loging In ....");
 })
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
