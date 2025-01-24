50d9ed48d133c3e077e1e0dcfd9f6362505c0706
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-01-24 14:08:25 +0200

    Server

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 //Let's an express js server
+const express=require('express');
+const app=express();
+app.get('/',(res,req)=>{
+     res.send("Hello Express")
+})
+app.listen(3000,()=>{
+     console.log("Server is running on http://localhost:3000")
+})
