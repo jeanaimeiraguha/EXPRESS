f6b0a6506e966886975ca262ce6e6b986dfdbf9a
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-01-24 14:19:42 +0200

    express

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-//Let's an express js server
+//Let's Create an express js server
 const express=require('express');
+const port=8080;
 const app=express();
-app.get('/',(res,req)=>{
-     res.send("Hello Express")
+app.get('/',(req,res)=>{
+     res.send("Hello Express");
 })
-app.listen(3000,()=>{
-     console.log("Server is running on http://localhost:3000")
+app.listen(port,()=>{
+     console.log("Server is running on http://localhost:8080")
 })
